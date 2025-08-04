18b6fc2cee341e9d6d76576388f87cd828b9640a
mayu <ykmshiss@gmail.com>
2025-08-05 08:59:54 +0900

    SQL Injection

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,33 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Movie;
-
+use Illuminate\Support\Facades\DB;
 
 class MovieController extends Controller
 {
-    public function index(Request $request)
-    {
-        $query = Movie::query();
+public function index(Request $request)
+{
+    $keyword = $request->input('keyword', '');
 
-        if ($request->has('is_showing') && $request->is_showing !== '') {
-            $query->where('is_showing', $request->is_showing);
-        }
+    if (str_contains($keyword, '__unsafe__')) {
+        $rawKeyword = str_replace('__unsafe__', '', $keyword);
 
-        if ($request->filled('keyword')) {
-            $keyword = $request->keyword;
-            $query->where(function ($q) use ($keyword) {
-                $q->where('title', 'like', "%{$keyword}%")
-                ->orWhere('description', 'like', "%{$keyword}%");
-            });
-        }
-
-        $movies = $query->paginate(20)->appends($request->query());
-
-        return view('movies.index', compact('movies'));
+        $movies = DB::table('movies')
+            ->whereRaw("title = '{$rawKeyword}'")
+            ->paginate(20)
+            ->appends($request->query());
+    } else {
+        $movies = DB::table('movies')
+            ->whereRaw("title = ?", [$keyword])
+            ->paginate(20)
+            ->appends($request->query());
     }
 
-
-
+    return view('movies.index', compact('movies'));
+}
     public function show($id)
     {
         $movie = Movie::with(['schedules' => function ($query) {
@@ -41,7 +38,4 @@ class MovieController extends Controller
 
         return view('movies.show', compact('movie', 'schedules'));
     }
-
-
-
 }
